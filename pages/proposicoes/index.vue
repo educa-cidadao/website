582ac9f3e86ae679af3fde6132f6d0bfd0c7e1ee
Matheus Leite <matheusjoselfm@gmail.com>
2018-08-18 21:29:56 -0300

    aceitando correcoes do git

--- pages/proposicoes/index.vue
+++ pages/proposicoes/index.vue
@@ -1,16 +1,8 @@
 <template>
-<<<<<<< HEAD
   <div>
     <nav-bar/>
     <list-projetos/>
   </div>
-=======
-<div>
-  <nav-bar/>
-  <list-projetos/>
-</div>
-
->>>>>>> 0c75f30e671f73594f402a082527a3bc1a1addf3
 </template>
 
 <script>
